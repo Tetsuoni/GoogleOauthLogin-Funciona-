d4dbc34060ff818a6615fc36cb49138221f3664e
nicolas teresa <nicolas.teresa1999.to@gmail.com>
2024-03-15 15:39:14 +0100

    Añadidos pasos que he seguido para la compatibilidad

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,28 @@ import { Capacitor } from '@capacitor/core';
 import GoogleButton from 'react-google-button';
 import { useState } from 'react';
 
+
+/*Pasos:
+1.  Crear el proyecto, configurar consent screen y credendiales en google Oauth.
+
+2.  Modificar archivos ->capacitor.config.ts e index.html
+
+3.  Código de la APP
+
+4.  Para compatibilidad con IOS (no he probado si funciona, no tengo el emulador y me he centrado en android) crear credencial, descargar el archivo,
+    y añadirlo al proyecto desde el emulador de IOS, en la carpeta IOS/App/App/, a la altura de Info. Al añadirlo seleccionar la opción 'copy items if needed', 
+    cambiarle el nombre al archivo por GoogleService-Info.plist.
+    Dentro del archivo, copiar contenido de 'REVERSE_CLIENT_ID', seleccionar APP, (la carpeta App, padre de otra carpeta App), ir a la pestaña de info, y pegar el código copiado
+    en URL Types -> URL Schemes.
+
+5.  Para compatibilidad en Android, ir a Android/App/src/main/java/MainActivity.java y modificarlo.
+    La credencial de Android es necesaria (Se necesita SHA 1), a pesar de no usarse como tal, debe relacionarla con la credencial web de forma interna.
+    Copiar clave de cliente web y pegarla en Android/App/src/res/values/strings.xml
+    
+6.  Ya ebería de funcionar la App!!
+
+
+*/
 function App() {
 
   interface User {
